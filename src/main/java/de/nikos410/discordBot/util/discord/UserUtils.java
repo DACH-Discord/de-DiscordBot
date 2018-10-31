d2851d948d346f3aeff7c90706c2bd6865a4b3d1
Nikos410 <nikos.epping@web.de>
2018-10-31 18:06:48 +0100

    UserUtils: Add JavaDoc

--- src/main/java/de/nikos410/discordBot/util/discord/UserUtils.java
+++ src/main/java/de/nikos410/discordBot/util/discord/UserUtils.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class UserUtils {
 
     /**
-     * Returns the highest role of a user.
+     * Get the highest role of a user.
      *
      * @param user The user whose highest role will be returned.
      * @param guild The guild for which to get the user's highest role.
@@ -30,10 +30,26 @@ public class UserUtils {
         return topRole;
     }
 
+    /**
+     * Check if a user has a role on a guild.
+     *
+     * @param user The user who to check for the role.
+     * @param role The role for which to check.
+     * @param guild The guild on which to check the user for the role.
+     * @return True if the user has the given role on the given guild, otherwise false.
+     */
     public static boolean hasRole(final IUser user, final IRole role, final IGuild guild) {
         return hasRole(user, role.getLongID(), guild);
     }
 
+    /**
+     * Check if a user has a role on a guild.
+     *
+     * @param user The user who to check for the role.
+     * @param roleID The ID of the role for which to check.
+     * @param guild The guild on which to check the user for the role.
+     * @return True if the user has a role with the given ID on the given guild, otherwise false.
+     */
     public static boolean hasRole(final IUser user, final long roleID, final IGuild guild) {
         final List<IRole> roles = user.getRolesForGuild(guild);
 
