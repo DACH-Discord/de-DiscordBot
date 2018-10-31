d9ef6ce039d5e12e3715e27ecad9fdd525ce7609
Nikos410 <nikos.epping@web.de>
2018-10-31 13:37:28 +0100

    Authorization: Add and improve JavaDoc comments

--- src/main/java/de/nikos410/discordBot/util/discord/Authorization.java
+++ src/main/java/de/nikos410/discordBot/util/discord/Authorization.java
@@ -7,11 +7,18 @@ import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.util.DiscordException;
 
 /**
- * Authorizes the Bot and returns Instance
+ * Contains helper method to authorize and log in a bot
  */
 public class Authorization {
     private final static Logger log = LoggerFactory.getLogger(Authorization.class);
 
+    /**
+     * Create a client instance using a token as the login information.
+     *
+     * @param token The token to use when authorizing.
+     * @param login Choose whether to log in the client after authorizing.
+     * @return The created client instance.
+     */
     public static IDiscordClient createClient(final String token, final boolean login) {
         final ClientBuilder clientBuilder = new ClientBuilder();
         clientBuilder.withToken(token);
