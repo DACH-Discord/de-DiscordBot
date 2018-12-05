f780059995b757ffdb9db6a4e3127a170a24d8bc
Nikos <nikos.epping@web.de>
2018-12-05 21:38:14 +0100

    Authorization: Remove unused imports

--- src/main/java/de/nikos410/discordbot/util/discord/Authorization.java
+++ src/main/java/de/nikos410/discordbot/util/discord/Authorization.java
@@ -1,7 +1,5 @@
 package de.nikos410.discordbot.util.discord;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.util.DiscordException;
