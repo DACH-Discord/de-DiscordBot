191c6b47bcde0f5992db2987668ed3935024d7d4
Nikos <nikos.epping@web.de>
2018-12-05 21:47:08 +0100

    GameStats: Remove unused import

--- src/main/java/de/nikos410/discordbot/modules/GameStats.java
+++ src/main/java/de/nikos410/discordbot/modules/GameStats.java
@@ -5,7 +5,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import de.nikos410.discordbot.DiscordBot;
 import de.nikos410.discordbot.exception.InitializationException;
