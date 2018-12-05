970fba88e878b23627cdca938a5aed850fb2c492
Nikos410 <nikos.epping@web.de>
2018-12-05 18:28:11 +0100

    DiscordBot.parseParameters: Use isEmpty to check whether list is empty

--- src/main/java/de/nikos410/discordbot/DiscordBot.java
+++ src/main/java/de/nikos410/discordbot/DiscordBot.java
@@ -520,7 +520,7 @@ public class DiscordBot {
         }
 
         // If passContext is true, append possible additional words to last parameter
-        if (passContext && contentPartIndex < contentParts.length && parameters.size() > 0) {
+        if (passContext && contentPartIndex < contentParts.length && !parameters.isEmpty()) {
             final StringBuilder builder = new StringBuilder();
             final String last = parameters.removeLast();
             builder.append(last);
